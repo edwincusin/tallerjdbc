bd9671082bddb21a115e60fc5a443a3b72425b72
EDWINCSN <c.edwin32@hotmail.com>
2026-05-18 22:03:16 -0500

    EGCA: Clase SelectVehiculo funcionando

---
+++ src/main/java/com/krakedev/tallerjdbc/jdbc/SelectVehiculo.java
@@ -0,0 +1,60 @@
+package com.krakedev.tallerjdbc.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SelectVehiculo {
+
+	private static Logger log = LoggerFactory.getLogger(SelectVehiculo.class);
+	
+	public static void main(String[] args) {
+		
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs=null;
+
+		try {
+			con = Conexion.getConnection();
+			String sql = """
+					SELECT  * FROM vehiculos;
+										""";
+			ps = con.prepareStatement(sql);
+			rs=ps.executeQuery();
+			
+			while(rs.next()) {
+				
+				String placa = rs.getString("placa");
+				String marca = rs.getString("marca");
+				String modelo = rs.getString("modelo");
+				int anio = rs.getInt("anio");
+				double precio= rs.getDouble("precio");
+				String color = rs.getString("color");
+				boolean disponible = rs.getBoolean("disponible");
+				//placa, marca, modelo, anio, precio, color, disponible
+				log.info("PLACA: "+placa+", MARCA: "+marca+", "+", MODELO: "+modelo+", AÑO: "+anio+", PRECIO: "+precio+", COLOR: "+color+", DISPONIBLE: "+disponible);
+				
+			}
+			log.info("LISTA COMPLETA DE VEHICULOS");
+		} catch (Exception e) {
+			log.error("Error al consultar : " + e.getMessage());
+		} finally {
+			try {
+				if (con != null) {
+					con.close();
+					log.info("CONEXION FINALIZADA");
+				}					
+
+			} catch (SQLException e) {
+				log.error("Error al cerrar la conexion : " + e.getMessage());
+			}
+		}
+
+	}
+
+
+}
