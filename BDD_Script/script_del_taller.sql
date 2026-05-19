ace65f0602ceae9e69911b81546e77c79ba2cd7e
EDWINCSN <c.edwin32@hotmail.com>
2026-05-18 20:09:48 -0500

    EGCA: agrega en el script de la bdd : crear tabla

---
+++ BDD_Script/script_del_taller.sql
@@ -0,0 +1,19 @@
+
+--CREAR BASE DE DATOS
+CREATE DATABASE tallerjdbc;
+
+--CREAR TABLA vehiculos
+CREATE TABLE vehiculos(
+placa VARCHAR(10) PRIMARY KEY,
+marca VARCHAR(50) NOT NULL,
+modelo VARCHAR(50) NOT NULL,
+anio INT NOT NULL,
+precio DOUBLE PRECISION NOT NULL,
+
+color VARCHAR(30),
+disponible BOOLEAN NOT NULL
+);
+
+
+
+DROP TABLE vehiculos;
